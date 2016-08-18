67dfecebcfe7b64b45b3f764f71dac88b2c9cefa
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-18 20:01:12 +0100

    Update coveralls filter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
 require 'coveralls'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'app/channels'
+  add_filter 'app/jobs'
+  add_filter 'app/mailers'
+end
 Coveralls.wear!('rails')
+
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
