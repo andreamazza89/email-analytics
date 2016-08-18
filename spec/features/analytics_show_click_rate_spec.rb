88290584c1f6c8708687a716322035c911bbe0db
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-18 10:18:56 +0100

    Expand click and open features to show multiple email_types

--- spec/features/analytics_show_click_rate_spec.rb
+++ spec/features/analytics_show_click_rate_spec.rb
@@ -1,13 +1,17 @@
 feature 'Feature - analytics: click rate' do
   scenario 'Click rate is shown on analytics page, example 0' do
-    visit analytics_path 
-    expect(page).to have_content 'Click rate for Shipment emails is 0%'
-  end
 
-  scenario 'Click rate is shown on analytics page, example 1' do
-    create_email_events(quantity: 3, email_type: 'Shipment', event: 'send')
-    create_email_events(quantity: 1, email_type: 'Shipment', event: 'click')
+    test_email_types = ['Shipment', 'ReferAFriend', 'UserConfirmation']
+
+    test_email_types.each do |type|
+      create_email_events(quantity: 3, email_type: type, event: 'send')
+      create_email_events(quantity: 1, email_type: type, event: 'click')
+    end
+
     visit analytics_path 
-    expect(page).to have_content 'Click rate for Shipment emails is 33%'
+
+    test_email_types.each do |type|
+      expect(page).to have_content "Click rate for #{type} emails is 33%"
+    end
   end
 end
--- spec/features/analytics_show_open_rate_spec.rb
+++ spec/features/analytics_show_open_rate_spec.rb
@@ -1,13 +1,10 @@
 feature 'Feature - analytics: open rate' do
   scenario 'Open rate is shown on analytics page, example 0' do
-    visit analytics_path 
-    expect(page).to have_content 'Open rate for Shipment emails is 0%'
-  end
-
-  scenario 'Open rate is shown on analytics page, example 1' do
     create_email_events(quantity: 3, email_type: 'Shipment', event: 'send')
     create_email_events(quantity: 1, email_type: 'Shipment', event: 'open')
+
     visit analytics_path 
-    expect(page).to have_content 'Open rate for Shipment emails is 33%'
+
+    expect(page).to have_content "Open rate for Shipment emails is 33%"
   end
 end
