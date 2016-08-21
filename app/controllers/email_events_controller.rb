49c1b468f87e85165be10520d3a4ad133eacc6b9
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-21 19:23:58 +0100

    Rename EmailEvent#create_email_event parameter

--- app/controllers/email_events_controller.rb
+++ app/controllers/email_events_controller.rb
@@ -3,8 +3,8 @@ class EmailEventsController < ApplicationController
   protect_from_forgery with: :exception, except: :create
 
   def create
-    event = JSON.parse(request.body.read)
-    EmailEvent.create_email_event(event)
+    event_data = JSON.parse(request.body.read)
+    EmailEvent.create_email_event(event_data)
     render plain: 'OK'
   end
 
--- app/models/email_event.rb
+++ app/models/email_event.rb
@@ -1,10 +1,10 @@
 class EmailEvent < ApplicationRecord
 
-  def self.create_email_event(event)
-    address    = event['Address']
-    email_type = event['EmailType']
-    event      = event['Event']
-    timestamp  = event['Timestamp']
+  def self.create_email_event(event_data)
+    address    = event_data['Address']
+    email_type = event_data['EmailType']
+    timestamp  = event_data['Timestamp']
+    event      = event_data['Event']
     self.create(address: address, email_type: email_type, event: event, timestamp: timestamp)
   end
 
